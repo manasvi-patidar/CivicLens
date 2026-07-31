0ac84f4b0c728ff43153fd601fbb93bd408f936d
Manasvi Patidar <manasvipatidar05.mp@gmail.com>
2026-07-31 23:02:16 +0530

    feat(database): initialize Prisma and User model

---
+++ server/prisma/migrations/20260731172449_init/migration.sql
@@ -0,0 +1,20 @@
+-- CreateEnum
+CREATE TYPE "public"."UserRole" AS ENUM ('CITIZEN', 'VOLUNTEER', 'AUTHORITY', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "public"."UserRole" NOT NULL DEFAULT 'CITIZEN',
+    "reputation" INTEGER NOT NULL DEFAULT 0,
+    "isVerified" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
