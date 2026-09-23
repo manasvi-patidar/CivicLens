6f7b511c76fe66d0b97d207238ad671d93375e5f
Manasvi Patidar <manasvipatidar05.mp@gmail.com>
2026-09-23 23:44:58 +0530

    chore: remove temporary Cloudinary runtime logging

--- server/src/config/cloudinary.ts
+++ server/src/config/cloudinary.ts
@@ -6,10 +6,4 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-console.log("Cloudinary runtime config:", {
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key_last4: process.env.CLOUDINARY_API_KEY?.slice(-4),
-  api_secret_length: process.env.CLOUDINARY_API_SECRET?.length,
-});
-
 export default cloudinary;
