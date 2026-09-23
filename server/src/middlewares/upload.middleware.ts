d332d24780b380fc29993edbb90bb2cf7fed8df6
Manasvi Patidar <manasvipatidar05.mp@gmail.com>
2026-09-23 18:28:01 +0530

    revert: restore cloudinary public id

--- server/src/middlewares/upload.middleware.ts
+++ server/src/middlewares/upload.middleware.ts
@@ -7,6 +7,7 @@ const storage = new CloudinaryStorage({
   params: async (_req, file) => ({
     folder: "CivicLens/issues",
     allowed_formats: ["jpg", "jpeg", "png", "webp"],
+    public_id: `${Date.now()}-${file.originalname.split(".")[0]}`,
   }),
 });
 
